7eacb58eeda7d60f6e1cc6050a4b2f6bb93d2998
Simon Kurka <dev@simon.kurka.cc>
2016-02-24 20:30:23 +0100

    duplicate declaration error resolved

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,13 @@ class bird (
   String $router_id,
 ) inherits bird::params {
 
+  class { 'bird::install': } ->
+  class { 'bird::config': } ~>
+  class { 'bird::service': }
+
   contain bird::install
   contain bird::config
   contain bird::service
-
-  class { '::bird::install': } ->
-  class { '::bird::config': } ~>
-  class { '::bird::service': }
 
 }
 
